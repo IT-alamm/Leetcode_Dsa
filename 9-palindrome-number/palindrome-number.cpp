e6b80cbb0794d885efa362cce97a6043e25c1c0b
IT-alamm <ma9788181@gmail.com>
2025-08-08 11:14:49 +0530

    Time: 3 ms (45.70%) | Memory: 8.7 MB (40.87%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -1,29 +1,3 @@
-// class Solution {
-// public:
-//     bool isPalindrome(int x) {
-//         if(x<0){
-//             return false;
-
-//         }
-//         int n = x;
-//          int revNum = 0;
-//          while(n > 0){
-//             int d = n%10;
-//             revNum = revNum * 10 + d;
-//             n = n/10;
-//          }
-//          if(revNum == x){
-//             return true;
-            
-
-//          }else{
-//             return false;
-//          }
-
-
-//     }
-// };
-
 
 class Solution {
 public:
