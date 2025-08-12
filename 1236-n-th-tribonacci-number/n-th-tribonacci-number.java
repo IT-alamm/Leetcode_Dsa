4a49ec17cecc05c052c3ec82fb7048058964f762
IT-alamm <ma9788181@gmail.com>
2025-08-12 21:35:16 +0530

    Time: 0 ms (100.00%) | Memory: 40.4 MB (64.80%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.java
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.java
@@ -3,25 +3,19 @@ class Solution {
         if(n==0){
             return 0;
         } else if(n==1 || n==2){
-            return 1;
-             
+            return 1;   
         } else{
-
             int firstTerm = 0;
             int secondTerm = 1;
             int thirdTerm = 1;
-
-
             for(int i = 1; i<=n; i++){
                 int fourthTerm = firstTerm + secondTerm + thirdTerm;
 
                 firstTerm = secondTerm;
                 secondTerm = thirdTerm;
                 thirdTerm = fourthTerm;
-
             }
-            return firstTerm;
-            
+            return firstTerm; 
         }
     }
 }
