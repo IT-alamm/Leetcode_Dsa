64fb89d6173ee68852b29bddf471e0ce4b39d37e
IT-alamm <ma9788181@gmail.com>
2025-09-03 21:58:05 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (31.60%) - LeetSync

---
+++ 162-find-peak-element/find-peak-element.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+        // if only one element
+        if(nums.length == 1) return 0;
+
+        // check first element
+        if(nums[0] > nums[1]) return 0;
+
+        // check last element
+        if(nums[nums.length-1] > nums[nums.length-2]) 
+         return   nums.length-1;
+
+        // binary search
+        int start = 1;
+        int end = nums.length - 2;
+
+        while(start <= end){
+            int mid = (start + end) / 2;
+
+            if(nums[mid] > nums[mid-1] && nums[mid] > nums[mid+1]){
+                return mid;
+            } else if(nums[mid] < nums[mid+1]){
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return -1; // theoretically shouldn't reach here
+    }
+}
