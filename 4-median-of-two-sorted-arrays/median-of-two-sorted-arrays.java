9c9206530b4c4f148821e8c4f0585cd750436eec
IT-alamm <ma9788181@gmail.com>
2026-01-20 23:48:08 +0530

    Time: 2 ms (46.23%) | Memory: 49.1 MB (9.65%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,29 +1,30 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         int[] ans = merge(nums1, nums2);
-        if (ans.length % 2 == 0) {
-            double ans2 = (double) (ans[ans.length / 2] + ans[ans.length / 2 - 1]) / 2;
-            return ans2;
+        int n = ans.length;
+        if (n % 2 == 0) {
+           return (double) (ans[n / 2] + ans[n / 2 - 1]) / 2;
+           
         } else {
-            double ans2 = (double) ans[ans.length / 2];
-            return ans2;
+            return (double) ans[ans.length / 2];
+           
         }
     }
+
     public int[] merge(int[] arr1, int[] arr2) {
         int[] ans = new int[arr1.length + arr2.length];
         int p1 = 0, p2 = 0, p3 = 0;
-        while (p1 < arr1.length || p2 < arr2.length) {
-            int val1 = p1 < arr1.length ? arr1[p1] : Integer.MAX_VALUE;
-            int val2 = p2 < arr2.length ? arr2[p2] : Integer.MAX_VALUE;
-            if (val1 < val2) {
-                ans[p3] = val1;
-                p1++;
-            } else {
-                ans[p3] = val2;
-                p2++;
-            }
-            p3++;
+        while (p1 < arr1.length && p2 < arr2.length) {
+            if(arr1[p1]<arr2[p2])
+                ans[p3++] = arr1[p1++];
+            else
+                ans[p3++] = arr2[p2++];
         }
+        while(p1 < arr1.length)
+            ans[p3++] = arr1[p1++];
+        while(p2 < arr2.length)
+            ans[p3++] = arr2[p2++];
+
         return ans;
     }
 }
